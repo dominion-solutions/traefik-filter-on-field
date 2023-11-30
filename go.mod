1aace77b782fbb19cd8e5c04a2277ec5ea5d4dc8
Mark J. Horninger <mark.horninger@dominion.solutions>
2023-11-30 08:42:45 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/dominion-solutions-open-source/traefik-filter-on-field
+
+go 1.21.4
---
+++ plugin.go
@@ -0,0 +1,68 @@
+package filter_on_field
+
+import (
+	"net/http"
+)
+
+type Error struct {
+	msg  string
+	code int
+}
+
+type Config struct {
+	/**
+	 * The name of the field to filter on
+	 */
+	fieldName string
+	/**
+	 * The message to return when disallowed content is found.  Defaults to "Disallowed content"
+	 */
+	responseMessage string
+	/**
+	 * A group of regular expressions representing content that is not allowed
+	 */
+	disallowedContent []string
+}
+
+type FilterOnField struct {
+	next   http.Handler
+	config *Config
+	name   string
+}
+
+/**
+ * Create a new instance of the filter
+ * @param ctx The configuration for the filter
+ * @param next The next handler in the chain
+ * @param name The name of the filter
+ * @return A new instance of the filter
+ */
+func New(ctx *Config, next http.Handler, name string) (http.Handler, error) {
+	return &FilterOnField{
+		next:   next,
+		config: ctx,
+		name:   name,
+	}, nil
+}
+
+/**
+ * Apply the middleware to the request
+ * @param rw The response writer
+ * @param req The request
+ */
+func (f *FilterOnField) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	parameter, err := req.getParameter(f.config.fieldName)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte(err.Error()))
+		return
+	}
+	for _, pattern := range f.config.disallowedContent {
+		if pattern.MatchString(parameter) {
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte(f.config.responseMessage))
+			return
+		}
+	}
+	f.next.ServeHTTP(rw, req)
+}
